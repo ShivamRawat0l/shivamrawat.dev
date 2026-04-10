3d7b58e0b34192f5d959aa42cbd3a0905cb16045
Shivam Rawat <shivamrawat6670@gmail.com>
2026-04-10 23:05:29 +0530

    Nice portfolio website is created

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
-import UnoCSS from 'unocss/vite';
 
 export default defineConfig({
-	root: '.',
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, './src'), // Correctly points @ to the project root
-		},
-	},
-	plugins: [
-		UnoCSS(),
-		react({
-			babel: {
-				plugins: [['babel-plugin-react-compiler']],
-			},
-		}),
-	],
+	plugins: [react()],
 });
